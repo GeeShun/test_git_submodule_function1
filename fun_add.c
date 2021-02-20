f4001a2bd9d2e49ee73a555c2d2f80235f2a1878
shun <mvshun@gmail.com>
2021-02-20 12:38:14 +0800

    add fun_add

---
+++ fun_add.c
@@ -0,0 +1,6 @@
+#include "fun_add.h"
+
+int Add(const int a, const int b)
+{
+    return a + b;
+}
---
+++ fun_add.h
@@ -0,0 +1,6 @@
+#ifndef __FUN_ADD_H__
+#define __FUN_ADD_H__
+
+int Add(const int, const int);
+
+#endif
